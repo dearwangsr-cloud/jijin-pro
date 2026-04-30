f46ae8809504a9a5bb86f43f1c0b7c152fabf1d4
Dengke <dearwangsr@gmail.com>
2026-04-30 10:25:41 +0800

    Delete fund-manager-pro directory

--- fund-manager-pro/public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='fund-pro-v1';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{clients.claim();});
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-   e.respondWith(fetch(e.request).catch(()=>caches.match('/index.html')))
- }
-});
